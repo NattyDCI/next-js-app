671fda4966eadaa24a7dcceba2729980463bffdd
NathaliaPadron <nbavutti@gmail.com>
2024-10-27 12:34:47 +0100

    configuration

---
+++ app/awesome/page.js
@@ -0,0 +1,11 @@
+// pages in nextJS are just react components
+
+export default function AwesomePage() {
+    return (
+        <main>
+            <h1>NextJS Is Awesome!</h1>
+            
+        </main>
+        );
+
+}
